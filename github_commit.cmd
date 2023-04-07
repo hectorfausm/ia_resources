bcc88405367231b59014563b976ef69c69e6543d
Faus Martinez <hfaus@sia.es>
2023-04-07 11:00:18 +0200

    ejecución con impulso

--- github_commit.cmd
+++ github_commit.cmd
@@ -1,6 +1,28 @@
-@ECHO off
+@echo off
+echo Ingrese el mensaje de commit: 
+set /p CommitString=
 
-SET /P CommitString=Please enter your commit string: 
 git add .
+if %errorlevel% neq 0 (
+    echo Error al añadir los cambios al repositorio local
+    pause
+    exit
+)
+
 git commit -m "%CommitString%"
+if %errorlevel% neq 0 (
+    echo Error al realizar el commit
+    pause
+    exit
+)
+
 git push -u origin main
+if %errorlevel% neq 0 (
+    echo Error al realizar el push
+    pause
+    exit
+)
+
+echo Proceso finalizado con exito.
+pause >nul
+exit
